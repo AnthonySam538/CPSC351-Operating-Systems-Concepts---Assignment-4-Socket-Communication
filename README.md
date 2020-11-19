5b2733f855c58417a32cefd7f382830ab93573e7
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2020-11-19 14:57:06 -0800

    now uses a thread
    The program now uses a thread to send messages. The idea is that, hopefully, I can create a second thread that will receive and display messages before this project is due tonight. In any case, I'll at least get a B on this project for getting this much done.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CPSC351-Operating-Systems-Concepts---Assignment-4-Socket-Communication
-Just two peers sending messages to each other.
+# CPSC351 Operating Systems Concepts - Assignment 4: Socket Communication
+Just two peers sending messages to each other. Though, I haven't actually figured that much out. At the moment, this peer is only able to send messages; it isn't able to receive and display messages.
--- cs351_assignment4_anthony_sam.cpp
+++ cs351_assignment4_anthony_sam.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
 #include <WinSock2.h>
 #include <Ws2tcpip.h>
 
@@ -6,8 +8,12 @@
 
 using namespace std;
 
-int main()
+mutex mtx;
+
+void sendMessage()
 {
+    mtx.lock();
+
     const char* sourceIP = "127.0.0.1";
     const char* destinationIP = "127.0.0.1";
     sockaddr_in source;
@@ -30,7 +36,7 @@ int main()
     destination.sin_port = htons(3514);
 
     bind(mySocket, (sockaddr*)&source, sizeof(source));
-    
+
     //The rest of the program
     const short maximumMessageLength = 2048;
     const char terminationString[] = "--exit";
@@ -39,8 +45,10 @@ int main()
     while (true)
     {
         cout << "Your message (enter \"--exit\" to exit the program): ";
+        mtx.unlock();
         cin.getline(myMessage, maximumMessageLength);
 
+        mtx.lock();
         //if the user did not enter the termination string
         if (strcmp(myMessage, terminationString))
             sendto(mySocket, myMessage, strlen(myMessage), 0, (sockaddr*)&destination, sizeof(destination));
@@ -52,7 +60,20 @@ int main()
             closesocket(mySocket);
             WSACleanup();
 
-            return 0;
+            mtx.unlock();
+
+            return;
         }
     }
+}
+
+//void receiveMessage()
+//{}
+
+int main()
+{
+    thread sendThread(sendMessage);
+    sendThread.join();
+
+    return 0;
 }
