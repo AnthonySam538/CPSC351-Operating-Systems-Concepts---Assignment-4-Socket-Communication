b2d6751270048eeed4eba943f65e10a8c95682c6
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2020-11-19 14:32:14 -0800

    Add files via upload

---
+++ cs351_assignment4_anthony_sam.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+    const char* sourceIP = "127.0.0.1";
+    const char* destinationIP = "127.0.0.1";
+    sockaddr_in source;
+    sockaddr_in destination;
+    WSAData data;
+
+    //Initialize the Windows Socket
+    WSAStartup(MAKEWORD(2, 2), &data);
+
+    //Create our sender socket to send messages
+    SOCKET mySocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    //Bind the socket to the specified IP address
+    source.sin_family = AF_INET;
+    inet_pton(AF_INET, sourceIP, &source.sin_addr.s_addr);
+    source.sin_port = htons(0);
+
+    destination.sin_family = AF_INET;
+    inet_pton(AF_INET, destinationIP, &destination.sin_addr.s_addr);
+    destination.sin_port = htons(3514);
+
+    bind(mySocket, (sockaddr*)&source, sizeof(source));
+    
+    //The rest of the program
+    const short maximumMessageLength = 2048;
+    const char terminationString[] = "--exit";
+    char myMessage[maximumMessageLength];
+
+    while (true)
+    {
+        cout << "Your message (enter \"--exit\" to exit the program): ";
+        cin.getline(myMessage, maximumMessageLength);
+
+        //if the user did not enter the termination string
+        if (strcmp(myMessage, terminationString))
+            sendto(mySocket, myMessage, strlen(myMessage), 0, (sockaddr*)&destination, sizeof(destination));
+        //if the user entered the termination string
+        else
+        {
+            cout << "Terminating the program...";
+
+            closesocket(mySocket);
+            WSACleanup();
+
+            return 0;
+        }
+    }
+}
